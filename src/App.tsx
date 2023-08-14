c62f987672b5711a56bb8c80ed3f53443004378b
walacefex <walacefex01@gmail.com>
2023-08-14 18:26:40 -0300

    Learning use useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 
 function App() {
+  function useEffectCallback() {
+    console.log('Mountou');
+    return () => {
+      console.log('Desmontou');
+    };
+  }
 
-  return (
-    <>
+  React.useEffect(useEffectCallback, []);
 
-      
-    </>
-  )
+  return <div>React</div>;
 }
 
-export default App
+export default App;
