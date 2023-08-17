2a5330016743a62b855cc641727470f2906a60fc
walacefex <walacefex01@gmail.com>
2023-08-16 21:21:24 -0300

    Creating Hook from Fetch in Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import React from 'react'
+import useFetch from './hooks/UseFetch'
+
+type Product = {
+  id: string;
+  nome: string;
+  preco: number;
+  quantidade: number;
+  descricao: string;
+  internacional: boolean;
+}
 
 function App() {
+  const [id, setId] = React.useState('p001')
+  const products = useFetch<Product[]>('https://data.origamid.dev/produtos')
+  const product = useFetch<Product>(`https://data.origamid.dev/produtos/${id}`)
 
   return (
-    <>
-
+    <section className='flex'>
+      <div>
+       {products.data && products.data.map(product => (
+        <button 
+          onClick={() => setId(product.id)}
+          style={{ fontSize:"1rem" }}
+          key={product.id}
+          >
+            {product.nome}
+          </button>
+       ))}
+      </div>
+      <div>
+        {products.loading && <p>Carregando...</p>}
+        {product.data && (
+          <ul>
+            <li> ID: {product.data.id}</li>
+            <li> Nome: {product.data.nome}</li>
+            <li> Descrição: {product.data.descricao}</li>
+            <li> Quantidade: {product.data.quantidade}</li>
+            <li> Preço: {product.data.preco}</li>
+          </ul>
+        )}
+      </div>
       
-    </>
+    </section>
   )
 }
 
---
+++ src/hooks/UseFetch.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function useFetch<T>(url: RequestInfo | URL , options?: RequestInit) {
+  const [data, setData] = React.useState<T | null> (null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState<string | null>(null)
+
+  const optionRef = React.useRef(options)
+  optionRef.current = options
+
+  React.useEffect(() => {
+    const controller = new AbortController();
+    const {signal} = controller;
+
+    const fetchData = async () => {
+      setLoading(true)
+      setData(null)
+      try {
+        const response = await fetch(url, {
+         signal,
+         ...optionRef.current
+        })
+        if(!response.ok) throw new Error(`Error: ${response.statusText}`)
+        const json = (await response.json()) as T;
+        if(!signal.aborted) setData(json);
+      } catch (error) {
+        if(!signal.aborted && error instanceof Error) setError(error.message)
+      } finally {
+        if(!signal.aborted) setLoading(false)
+      }
+    }
+    fetchData()
+    return () => controller.abort()
+  }, [url])
+
+  return { data, loading, error }
+}
+
+export default useFetch
+
