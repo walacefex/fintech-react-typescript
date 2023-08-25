045232384f40f3ff37a6e49608f2a04cca315425
walacefex <walacefex01@gmail.com>
2023-08-25 14:28:50 -0300

    Config routes in projet, adding routes of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./Components/Header"
 import Sidenav from "./Components/Sidenav"
 import { DataContextProvider } from "./Context/DataContext"
 import Resume from "./Pages/Resume"
 import Vendas from "./Pages/Vendas"
+import Venda from "./Pages/Venda"
 import "./Style.css"
 function App() {
 
   return (
+    <BrowserRouter>
       <DataContextProvider>
         <div className='container'>
           <Sidenav />
           <main>
             <Header />
-            <Resume />
-            <Vendas />
+            <Routes>
+              <Route path='/' element={<Resume />} />
+              <Route path='/vendas' element={<Vendas />} />
+              <Route path='/vendas/:id' element={<Venda />} />
+            </Routes>          
           </main>
         </div>
       </DataContextProvider>
+    </BrowserRouter>
   )
 }
 
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import DateRange from '../DataRange'
 import Months from '../Months'
+import { useLocation } from 'react-router-dom'
 
 const Header = () => {
   const [title, setTitle] = React.useState('Resumo')
+  const location = useLocation()
+  React.useEffect(() => {
+    if(location.pathname === '/') {
+      setTitle('Resumo')
+      document.title = 'Fintech | Resumo'
+    } else if(location.pathname === '/vendas') {
+      setTitle('Vendas')
+      document.title = 'Fintech | Vendas'
+    }
+  }, [location])
 
   return (
     <header className='mb'>
